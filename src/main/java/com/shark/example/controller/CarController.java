a3def9fcedc8225845cf5eda1037fb376fc90ae2
Shark0 <lin22223344@gmail.com>
2021-03-22 23:16:55 +0800

    add transactional example

--- src/main/java/com/shark/example/controller/CarController.java
+++ src/main/java/com/shark/example/controller/CarController.java
@@ -4,10 +4,13 @@ package com.shark.example.controller;
 import com.shark.example.dao.example.pojo.CarDo;
 import com.shark.example.dao.example.repository.CarRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,5 +22,21 @@ public class CarController {
     @GetMapping()
     public List<CarDo> read() {
         return carRepository.findAll();
+    }
+
+
+    @Transactional(transactionManager = "exampleTransactionManager", rollbackFor = Exception.class)
+    @PostMapping()
+    public CarDo create(boolean exception) {
+        CarDo carDo = new CarDo();
+        carDo.setName("Test");
+        carDo.setPrice(99);
+        carDo.setCreateTime(new Date());
+        carDo = carRepository.save(carDo);
+        if(exception) {
+            int value = 1 / 0;
+            System.out.println("value: " + value);
+        }
+        return carDo;
     }
 }
--- src/main/java/com/shark/example/dao/example/pojo/CarDo.java
+++ src/main/java/com/shark/example/dao/example/pojo/CarDo.java
@@ -2,20 +2,20 @@ package com.shark.example.dao.example.pojo;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "car")
 public class CarDo implements Serializable {
-
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private Integer price;
+
+    @Column(name = "create_time")
+    private Date createTime;
 }
